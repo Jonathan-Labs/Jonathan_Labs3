0b6c6c269873cd9db6ef45a2ef223e6216807fd9
Jonathan-Labs <labistejonathanjr@gmail.com>
2024-12-28 22:40:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menuToggle").addEventListener("click", function() {
+    const menuList = document.getElementById("menuList");
+    menuList.classList.toggle("active");
+});
